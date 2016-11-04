61a5c06615811d07a2fb2ffe3ee8e925517dac7a
Leonid Cheremshantcev <plastic.life.ln@gmail.com>
2016-11-04 13:22:53 +0400

    Add new feature

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask, jsonify
+from flask import abort
 from flask import json
+from flask import make_response
+from flask import request
 
 from model.Task import Task
 from model.TaskList import TaskList
@@ -24,6 +27,30 @@ def index():
 @app.route(main_url + 'tasks', methods=['GET'])
 def get_tasks():
     return json.dumps({'tasks': tasks.getTasks()}, ensure_ascii=False)
+
+
+@app.route(main_url + 'tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    if task_id < 0 or task_id > tasks.count:
+        abort(404)
+    return json.dumps({'task': tasks.task_list[task_id].get_dict()}, ensure_ascii=False)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@app.route(main_url + 'tasks', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = Task(tasks.task_list[-1].task_id + 1,
+                request.json['title'],
+                request.json.get('description', ""),
+                False)
+    tasks.addTask(task)
+    return json.dumps({'task': task.get_dict()}, ensure_ascii=False),201
 
 
 if __name__ == '__main__':
--- model/TaskList.py
+++ model/TaskList.py
@@ -4,12 +4,16 @@ from model.Task import Task
 class TaskList:
     def __init__(self):
         self.task_list = []
+        self.count = 0
 
     def addTask(self, task: Task):
         self.task_list.append(task)
+        self.count += 1
 
     def getTasks(self):
         ans = []
         for t in self.task_list:
             ans.append(t.get_dict())
         return ans
+
+
